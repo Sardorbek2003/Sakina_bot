6fc0d6b42ddc85326e30597d9d9c67d1cf276c90
Abdulaziz_java <rakhimoff1505@gmail.com>
2024-08-25 15:57:36 +0500

    I added Hijri date successfully

--- Bot/src/main/java/uz/pdp/SakinaBot.java
+++ Bot/src/main/java/uz/pdp/SakinaBot.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import uz.pdp.engYaqinNamoz.EngYaqinNamoz;
+import uz.pdp.hijriSana.HijriSana;
 import uz.pdp.kitoblar.Kitoblar;
 import uz.pdp.namozVaqtlar.NamozVaqtlari;
 import uz.pdp.namozVaqtlar.NamozVaqtlariInline;
@@ -14,7 +15,6 @@ import uz.pdp.namozVaqtlar.NamozVaqtlariInline;
 import java.util.Date;
 
 public class SakinaBot extends TelegramLongPollingBot {
-
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
@@ -40,6 +40,13 @@ public class SakinaBot extends TelegramLongPollingBot {
                     SendMessage sendMessage = EngYaqinNamoz.getNextPrayer(message.getChatId());
                     execute_(sendMessage);
                 }
+                else if (text.equals("Xijriy Yil Xisobi\uD83D\uDCC6")) {
+                    String hijriDate = HijriSana.fetchHijriDate();
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(message.getChatId());
+                    sendMessage.setText(hijriDate);
+                    execute_(sendMessage);
+                }
             }
         } else if (update.hasCallbackQuery()) {
             CallbackQuery callbackQuery = update.getCallbackQuery();
@@ -60,17 +67,14 @@ public class SakinaBot extends TelegramLongPollingBot {
             }
         }
     }
-
     @Override
     public String getBotUsername() {
         return "library_0304_bot";  // Username qismini qaytarish
     }
-
     @Override
     public String getBotToken() {
         return "7288467792:AAHKQse3hGoUFjZD2ehOTescDE_5rrlod-w";  // Tokenni to'g'ri kiriting
     }
-
     private void execute_(SendMessage sendMessage) {
         try {
             execute(sendMessage);
